c03601223697bec7d1af6e80fe6f576fe5520618
K Bindesh <30317164+kbindesh@users.noreply.github.com>
2024-03-09 07:23:27 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,10 +1,5 @@
-/**
- * Hello world Program
- */
-public class helloWorldApp
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World.." );
+public class Hello {
+    public static void main(String args[]){
+        System.out.println("Jenkins is awesome");
     }
 }
